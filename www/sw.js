5ea88df201dc5466aa616d69285b49ff82de4e97
1400621504-lang <1400621504@qq.com>
2026-05-08 15:49:26 +0800

    feat: 添加 Capacitor Android 打包 + GitHub Actions 自动构建 APK

---
+++ www/sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'shooter-v14';
+const URLS = [
+  './',
+  'index.html',
+  'manifest.json',
+  'icon-180.png',
+  'icon-192.png',
+  'icon-512.png',
+  'bgm/不再曼波.mp3',
+  'bgm/打火基.mp3',
+  'bgm/boss战bgm.mp3',
+  'logo.png',
+  'boss-imgs/hiss1.mp3', 'boss-imgs/hiss2.mp3',
+  'boss-imgs/hiss3.mp3',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(URLS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('message', e => {
+  if (e.data && e.data.type === 'SKIP_WAITING') self.skipWaiting();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
